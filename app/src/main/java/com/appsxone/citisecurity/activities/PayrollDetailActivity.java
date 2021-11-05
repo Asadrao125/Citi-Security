5dde07e03bc411a1bb04c97e47c94d62aeef2dba
Asad Rao <raoasad125@gmail.com>
2021-11-05 16:28:52 +0500

    Till, Version 1.01

--- app/src/main/java/com/appsxone/citisecurity/activities/PayrollDetailActivity.java
+++ app/src/main/java/com/appsxone/citisecurity/activities/PayrollDetailActivity.java
@@ -70,7 +70,6 @@ public class PayrollDetailActivity extends AppCompatActivity implements ApiCallb
                 getBillDetails(BillId);
             }
         });
-
     }
 
     private void getBillDetails(String BillId) {
