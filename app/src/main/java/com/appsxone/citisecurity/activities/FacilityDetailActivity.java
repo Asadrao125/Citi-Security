8426ece4fcf0b8c2c3dfd52e80f5402742ff61bf
Asad Rao <raoasad125@gmail.com>
2021-11-12 16:59:46 +0500

    Till, Version 1.05

--- app/src/main/java/com/appsxone/citisecurity/activities/FacilityDetailActivity.java
+++ app/src/main/java/com/appsxone/citisecurity/activities/FacilityDetailActivity.java
@@ -111,17 +111,14 @@ public class FacilityDetailActivity extends AppCompatActivity implements ApiCall
             List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
             for (NetworkInterface nif : all) {
                 if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
-
                 byte[] macBytes = nif.getHardwareAddress();
                 if (macBytes == null) {
                     return "";
                 }
-
                 StringBuilder res1 = new StringBuilder();
                 for (byte b : macBytes) {
                     res1.append(String.format("%02X:", b));
                 }
-
                 if (res1.length() > 0) {
                     res1.deleteCharAt(res1.length() - 1);
                 }
